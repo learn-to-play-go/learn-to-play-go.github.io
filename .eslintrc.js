f4dedb363ff63c4fdbca249885cce5780c86db48
learn-to-play-go <learn-to-play-go@users.noreply.github.com>
2020-02-14 19:57:27 +0300

    Move over to Nuxt.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
