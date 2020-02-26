b4676bcc905945b1d9baf13fac8b4b11d9fc4cad
learn-to-play-go <learn-to-play-go@users.noreply.github.com>
2020-02-26 14:08:03 +0300

    Add the description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'The best interactive way to learn the game of Go/Baduk/Weiqi. Rules, basic principles, and more...' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
